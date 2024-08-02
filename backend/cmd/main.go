2f7e4870f9cea9774f02b7cea88acc93e0b080f7
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-02 16:22:07 -0400

    add cors

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
     "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/cyril-ui-developer/july7-task-manager/backend/database"
 	"github.com/cyril-ui-developer/july7-task-manager/backend/routes"
@@ -13,7 +14,7 @@ func main() {
     database.Connect()
     database.Automigrate()
 	app := fiber.New()
-
+	app.Use(cors.New())
 	routes.Setup(app)
     log.Fatal(app.Listen(":4000"))
 }
