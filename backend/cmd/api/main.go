bcc354e8318a1f9dead7dbfba26343eb6eba7b63
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-02 11:21:17 -0400

    initial commit

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "log"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("July7 Task Manager API!")
+    })
+
+    log.Fatal(app.Listen(":4000"))
+}
