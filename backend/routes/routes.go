0620fedfc7d309741b8c2fceda4c976072ccd6c4
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-03 12:15:20 -0400

    rename controllers to handlers

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,13 +2,13 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/cyril-ui-developer/july7-task-manager/backend/controllers"
+	"github.com/cyril-ui-developer/july7-task-manager/backend/handlers"
 )
 
 func Setup(app *fiber.App) {
 	api := app.Group("/api/v1")
 
-	api.Get("/tasks", controllers.GetAllTasks)
-	api.Post("/tasks", controllers.CreateTask)
+	api.Get("/tasks", handlers.GetAllTasks)
+	api.Post("/tasks", handlers.CreateTask)
 
 }
