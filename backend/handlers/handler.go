d8ade6e76afd14033bd4d16fbec197a65983c94e
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-03 13:03:44 -0400

    enhance createtask method

--- backend/handlers/handler.go
+++ backend/handlers/handler.go
@@ -21,68 +21,19 @@ func GetAllTasks(c *fiber.Ctx) error {
     return c.JSON(tasks)
 }
 
-// func GetAllTasks(c *fiber.Ctx) error {
-// 	var tasks []models.Task
-// 	err := database.DB.Debug().Find(&tasks)
-// 	if err != nil {
-// 	 c.Status(500).JSON(&fiber.Map{
-//             "message": err,
-//             "status": "failed to fetch tasks",
-//           })
-    
-//     }
-// 	return c.JSON(tasks)
-// }
-
 func CreateTask(c *fiber.Ctx) error {
     task := new(models.Task)
     if err := c.BodyParser(task); err != nil {
-        return c.Status(400).JSON(fiber.Map{
-            "error": "Bad request: Cannot parse JSON",
+        return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+            // Bad practice: Do not expose the actual error to the client in production but okay for development.
+            "error": "Cannot parse JSON: " + err.Error(),
         })
     }
-	result := database.DB.Debug().Create(&task)
-    if result.Error != nil {
-        return c.Status(500).JSON(fiber.Map{
-            "error": "Internal server error: Cannot create task",
+	err := database.DB.Debug().Create(&task)
+    if err.Error != nil {
+        return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+            "error": "Cannot create task: " + err.Error.Error(),
         })
     }
     return c.JSON(task)
 }
-
-// func CreateTask(c *fiber.Ctx) error {
-//     task := new(models.Task)
-//     if err := c.BodyParser(task); err != nil {
-// 		log.Println("one", err)
-// 		return c.Status(400).JSON(&fiber.Map{
-//             "status": "failed",
-//             "message": err,
-//           })
-        
-//     }
-//    err := database.DB.Debug().Create(&task)
-//    log.Println("rwo", err)
-// 	if err != nil {
-		
-// 		return c.Status(500).JSON(&fiber.Map{
-//             "status": "failed",
-//             "message": err,
-//           })
-        
-//     }
-//     //return c.JSON(task)
-// 	return c.JSON(&fiber.Map{
-//         "status": "success",
-//         "message": "Task successfully created",
-//         "product": task,
-//       });
-// 	//    err != nil {
-// 	// 	log.Println("three", err)
-// 	// 	return c.Status(500).JSON(&fiber.Map{
-//     //         "status": "failed",
-//     //         "message":  "Error creating task",
-//     //       })
-        
-//     // }
-// 	// return nil
-//     }
