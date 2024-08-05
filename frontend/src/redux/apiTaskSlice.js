313a03524a2514f6564b9b87fcc70d2d40231fd1
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-04 21:02:04 -0400

    add coments to apiTasksSlice

--- frontend/src/redux/apiTaskSlice.js
+++ frontend/src/redux/apiTaskSlice.js
@@ -6,22 +6,22 @@ const baseUrl=  process.env.NODE_ENV === 'development' ? 'http://localhost:4000/
 export const apiTasksSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl }),
-  tagTypes: ["Tasks"],
-  endpoints: (builder) => ({
+  tagTypes: ["Tasks"], // This is a tag type that will be used to invalidate the cache
+  endpoints: (builder) => ({ // This is where we define our endpoints
     getTasks: builder.query({
       query: () => "/tasks",
       transformResponse: (res) => res.reverse(),
-      providesTags: ["Tasks"],
+      providesTags: ["Tasks"], // This is a tag that will be used to invalidate the cache. Meaning that any mutation that invalidates the "Tasks" tag will cause this query to refetch.
     }),
-    addTask: builder.mutation({
+    addTask: builder.mutation({ // This is a mutation that will be used to add a new task
       query: (task) => ({
         url: "/tasks",
         method: "POST",
         body: task,
       }),
-      invalidatesTags: ["Tasks"],
+      invalidatesTags: ["Tasks"], // This is a tag that will be used to invalidate the cache
     }),
-    updateTaskCompleted: builder.mutation({
+    updateTaskCompleted: builder.mutation({ // This is a mutation that will be used to update the completed status of a task
       query: (task) => ({
         url: `/tasks/${task.id}/completed`,
         method: "PATCH",
@@ -29,7 +29,7 @@ export const apiTasksSlice = createApi({
       }),
       invalidatesTags: ["Tasks"],
     }),
-    deleteTask: builder.mutation({
+    deleteTask: builder.mutation({ // This is a mutation that will be used to delete a task
       query: (id ) => ({
         url: `/tasks/${id}`,
         method: "DELETE",
@@ -40,8 +40,9 @@ export const apiTasksSlice = createApi({
   }),
 });
 
+// Export hooks for usage in functional components, which are auto-generated based on the defined endpoints
 export const {
-  useGetTasksQuery,
+  useGetTasksQuery, 
   useAddTaskMutation,
   useUpdateTaskCompletedMutation,
   useDeleteTaskMutation,
