e27c6700426f4889e1989659d91e58b044bd1757
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-08 15:45:40 -0400

    gorm.Modelto Add fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`

--- backend/database/database.go
+++ backend/database/database.go
@@ -16,11 +16,15 @@ var DB *gorm.DB
 // Use gorm ORM to connect to the mysql database and migrate the schema.The connections in the pool are automatically closed. 
 func Connect() error{
 	var err error
-	dns := fmt.Sprintf("%s@tcp(%s:%s)/%s", url.UserPassword(config.Config("DB_USERNAME"), config.Config("DB_PASSWORD")), config.Config("DB_HOST"), config.Config("DB_PORT"), config.Config("DB_NAME"))
+
+	// ?parseTime=true to the db dns string to parse time.Time correctly from the database.
+	dns := fmt.Sprintf("%s@tcp(%s:%s)/%s?parseTime=true", url.UserPassword(config.Config("DB_USERNAME"), config.Config("DB_PASSWORD")), config.Config("DB_HOST"), config.Config("DB_PORT"), config.Config("DB_NAME"))
+	
 	DB, err = gorm.Open(mysql.Open(dns), &gorm.Config{})
 	if err != nil {
 		panic("could not connect to database")
 	}
+	
 	err =  DB.AutoMigrate(models.Task{})
 	if err != nil {
 		panic("could not connect to database")
--- backend/models/task.go
+++ backend/models/task.go
@@ -1,20 +1,13 @@
 package models
 
 import (
-	"github.com/google/uuid"
 	"gorm.io/gorm"
    )   
 
-type Task struct {
-	ID           uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	Title       string `json:"title" gorm:"default:title not provided"`
-	Description string `json:"description" gorm:"default:description not provided"`
-	Completed   bool `json:"completed" gorm:"default:false"`
-	Active      bool `json:"active" gorm:"default:false"`
-}
-
-// BeforeCreate will set a UUID rather than numeric ID.
-func (task *Task) BeforeCreate(tx *gorm.DB) (err error) {
-    task.ID = uuid.New()
-    return
+   type Task struct {
+	gorm.Model // Add fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`
+    Title       string    `json:"title" gorm:"default:title not provided"`
+    Description string    `json:"description" gorm:"default:description not provided"`
+    Completed   bool      `json:"completed" gorm:"default:false"`
+    Active      bool      `json:"active" gorm:"default:false"`
 }
