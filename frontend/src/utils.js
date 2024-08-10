4183b2fd53deb835c973b656852d8eb99fbe5bd4
cyril-ui-developer <cyril.ajieh@gmail.com>
2024-08-09 20:49:01 -0400

    clean uplist tasks page

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,7 +1,4 @@
 export const toSentenceCase = str => str.slice(0, 1).toUpperCase() + str.slice(1);
 
-export const formatToDateString = (date) => {
-    const [month, day, year] = new Date(date).toDateString().split(' ');
-    const twoDigitYear = year.slice(-2);
-    return `${month} ${day}, ${twoDigitYear}`;
-}
+export const formatToDateString = (hireDate) =>
+    new Date(hireDate).toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' });
